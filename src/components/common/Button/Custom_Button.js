111ce163b3872fb588247db50465c4f516b9bb46
quocsonnguyen <quocsonnguyen2010@gmail.com>
2021-10-25 15:22:04 +0700

    :sparkles: edit style in LoginPage

--- src/components/common/Button/Custom_Button.js
+++ src/components/common/Button/Custom_Button.js
@@ -1,12 +1,11 @@
-
 import React from 'react'
 import styles from '../Button/CustomButton.module.css'
 const Custom_Button = (props) => {
-    
+
     if(props.variant==='fill_blue'){
         return (
             <>
-            <button className={styles.fill_blue}>{props.text}</button>
+            <button className={styles.fill_blue}><b>{props.text}</b></button>
             </>
         )
     }
--- src/components/modules/Login/LoginPage.js
+++ src/components/modules/Login/LoginPage.js
@@ -5,7 +5,6 @@ import styles from '../Login/LoginPage.module.css'
 import { CustomButton } from '../../common';
 import { GoogleLogin } from 'react-google-login';
 
-
 const responseGoogle = (response) => {
     console.log(response);
     console.log(response.profileObj);
@@ -24,23 +23,23 @@ function LoginPage() {
                     </Col>
 
                     <Col md={4} className={styles.Login_content}>
-                        <h2 className={styles.Login_title}>Hệ Thống Thông Tin <br />Sinh Viên</h2>
+                        <h2 className={styles.Login_title}><b>Hệ Thống Thông Tin <br />Sinh Viên</b></h2>
 
                         <Form action='/login' method='POST' className={styles.Login_form}>
 
                             <Form.Group className="mb-3" controlId="formEmail">
-                                <Form.Label>Email </Form.Label>
+                                <Form.Label><b>Email</b> </Form.Label>
                                 <Form.Control type="email" placeholder="Enter email" className={styles.Login_form_input} />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formPassword">
-                                <Form.Label>Password  </Form.Label>
+                                <Form.Label><b>Password</b>  </Form.Label>
                                 <Form.Control type="password" placeholder="Password" className={styles.Login_form_input} />
                             </Form.Group>
 
-                            <div>
+                            <div className={styles.Login_login_buttons}>
                                 <CustomButton variant='fill_blue' text='Đăng Nhập' className={styles.button22} />
-                                <div>
+                                <div className={styles.Login_google_login}>
                                     <GoogleLogin
                                         clientId="917753298000-r032b63avasjd0m4il2681eirlc1eoct.apps.googleusercontent.com"
                                         buttonText="Login With Google"
@@ -49,8 +48,6 @@ function LoginPage() {
                                         cookiePolicy={'single_host_origin'}
                                     />
                                 </div>
-           
-
                             </div>
                         </Form>
                     </Col>
